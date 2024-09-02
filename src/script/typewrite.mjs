fd8f3ad85714d4c9a9c3c21407125523b72a30c0
Vlad Jerca <vlad.jerca@gmail.com>
2024-09-03 00:56:09 +0200

    refactor: improve typewrite function to handle displaying all messages

--- src/script/typewrite.mjs
+++ src/script/typewrite.mjs
@@ -1,13 +1,46 @@
-export function typewrite(element, messages) {
-    const nextMessageDelay = 2500;
-    const characterTypeDelay = 1000 / 24;
+export function typewrite(element, messages, options = {}) {
+    const {
+        nextMessageDelay = 4500,
+        characterTypeDelay = 1000 / 30,
+        characterWipeDelay = 1000 / 60,
+    } = options;
+
 
     const state = {
         messageIndex: 0,
         characterIndex: 0,
         characterPrintTime: 0,
+        characterWipeTime: 0,
         messageQueueTime: 0,
+        isDoneTyping: false,
     };
+
+    function wipe(currentTime) {
+        const message = messages[state.messageIndex];
+
+        if (!message) {
+            return;
+        }
+
+        const isWipingCharacter = state.characterIndex > 0;
+
+        const elapsedCharacterWipingTime = currentTime - state.characterWipeTime;
+
+        if (!isWipingCharacter) {
+            element.textContent = '';
+            state.isDoneTyping = false;
+            state.characterWipeTime = 0;
+            state.messageIndex += 1;
+        }
+
+        if (elapsedCharacterWipingTime > characterWipeDelay && isWipingCharacter) {
+            element.textContent = message.slice(0, state.characterIndex - 1);
+            state.characterIndex--;
+            state.characterWipeTime = currentTime;
+        }
+
+        requestAnimationFrame(start);
+    }
 
     function type(currentTime) {
         const message = messages[state.messageIndex];
@@ -15,7 +48,7 @@ export function typewrite(element, messages) {
         if (!message) {
             return;
         }
-        
+
         const isPrintingCharacter = state.characterIndex < message.length;
 
         const elapsedCharacterPrintingTime = currentTime - state.characterPrintTime;
@@ -28,14 +61,22 @@ export function typewrite(element, messages) {
         }
 
         if (!isPrintingCharacter && elapsedMessagePritingTime >= nextMessageDelay) {
-            element.textContent = "";
-            state.characterIndex = 0;
-            state.messageIndex += 1;
+            state.isDoneTyping = true;
             state.messageQueueTime = currentTime;
         }
 
-        requestAnimationFrame(type);
+        requestAnimationFrame(start);
     }
 
-    requestAnimationFrame(type);
+    function start() {
+        const isDoneDisplayingAllMessages = messages.length - 1 === state.messageIndex;
+        if (state.isDoneTyping && isDoneDisplayingAllMessages) {
+            return;
+        }
+
+        const fn = state.isDoneTyping ? wipe : type;
+        requestAnimationFrame(fn);
+    }
+
+    start();
 }
