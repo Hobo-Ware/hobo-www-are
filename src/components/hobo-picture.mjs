5e749766c0cc43352a3b3161324d484786c4e85f
seferturan <seferturan85@gmail.com>
2024-09-18 21:59:28 +0200

    feat: hover effect on founders

--- src/components/hobo-picture.mjs
+++ src/components/hobo-picture.mjs
@@ -1,3 +1,37 @@
+function createPicture(founder, isFrame = false) {
+    const className = isFrame ? 'frame' : 'founder';
+    const assetPrefix = isFrame ? `frame_${founder}` : founder;
+
+    const picture = document.createElement('picture');
+    picture.addEventListener('contextmenu', e => e.preventDefault());
+    picture.classList.add(className);
+
+    const responsiveSizes = [
+        {
+            maxWidth: 480,
+            fileName: `${assetPrefix}_200.png`
+        },
+        {
+            maxWidth: 768,
+            fileName: `${assetPrefix}_400.png`
+        },
+    ];
+
+    responsiveSizes.forEach(({ maxWidth, fileName }) => {
+        const source = document.createElement('source');
+        source.setAttribute('media', `(max-width: ${maxWidth}px)`);
+        source.setAttribute('srcset', `assets/${founder}/${fileName}`);
+        picture.append(source);
+    });
+
+    const img = document.createElement('img');
+    img.setAttribute('srcset', `assets/${founder}/${assetPrefix}_600.png`);
+
+    picture.append(img);
+
+    return picture
+}
+
 export class HoboPicture extends HTMLElement {
     #isRendered = false;
 
@@ -12,38 +46,75 @@ export class HoboPicture extends HTMLElement {
         }
 
         const founder = this.getAttribute('founder');
+        const imgSize = this.getAttribute('image-size') || 'auto';
+        const animationDuration = this.getAttribute('animation-duration') || '0.5s';
+        const hoverSizeIncrease = this.getAttribute('hover-size-increase') || 5;
 
         const style = document.createElement('style');
-        style.textContent = `
-            picture {
-                display: flex;
 
-                img {
-                    width: var(--founder-size);
+        style.textContent = `
+            picture img {
+                width: ${imgSize};
+            }
+
+            .frame img {
+                position: absolute;
+
+                transition:
+                        width 0.3s,
+                        height 0.3s,
+                        margin-left 0.3s,
+                        margin-top 0.3s;
+                    
+                &:hover {
+                    transition:
+                        width ${animationDuration},
+                        height ${animationDuration},
+                        margin-left ${animationDuration},
+                        margin-top ${animationDuration};
+                    width: calc(100% + ${hoverSizeIncrease}%);
+                    height: auto;
+                    margin-left: -${hoverSizeIncrease / 2}%;
+                    margin-top: -${hoverSizeIncrease / 2}%;
+                }
+            }
+
+            .frame::before {
+                content: '';
+                position: absolute;
+                display: block;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                width: 0px;
+                height: 0px;
+                background: rgba(0, 0 ,0 , 0.2);
+                border-radius: 5%;
+                opacity: 0;
+            }
+
+            .frame:hover::before {
+                animation: founder-square-flash ${animationDuration};
+            }
+
+            @keyframes founder-square-flash {
+                40% {
+                    opacity: 1;
+                }
+                100% {
+                    width: ${imgSize};
+                    height: ${imgSize};
+                    opacity: 0;
                 }
             }
         `;
 
-        const picture = document.createElement('picture');
-        picture.addEventListener('contextmenu', e => e.preventDefault());
-
-        const source480 = document.createElement('source');
-        source480.setAttribute('media', '(max-width: 480px)');
-        source480.setAttribute('srcset', `assets/${founder}/${founder}_200.png`);
-
-        const source768 = document.createElement('source');
-        source768.setAttribute('media', '(max-width: 768px)');
-        source768.setAttribute('srcset', `assets/${founder}/${founder}_400.png`);
-
-        const img = document.createElement('img');
-        img.setAttribute('srcset', `assets/${founder}/${founder}_600.png`);
-
-        picture.append(source480);
-        picture.append(source768);
-        picture.append(img);
+        const pictureFounder = createPicture(founder);
+        const pictureFrame = createPicture(founder, true);
 
         this.shadowRoot.append(style);
-        this.shadowRoot.append(picture);
+        this.shadowRoot.append(pictureFrame);
+        this.shadowRoot.append(pictureFounder);
 
         this.#isRendered = true;
     }
