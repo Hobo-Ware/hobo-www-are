d1669057267e0797db181271bf4894137f263f46
Vlad Jerca <vlad.jerca@gmail.com>
2024-11-15 01:45:38 +0100

    fix(theme): render theme picker on mount to prevent transition

--- src/lib/features/theme/components/ThemeToggleIcon.svelte
+++ src/lib/features/theme/components/ThemeToggleIcon.svelte
@@ -17,43 +17,36 @@
 	/>
 </svelte:head>
 
-<button
-	class:theme-toggle--toggled={theme === Theme.Dark}
-	class:theme-toggle--mounted={mounted}
-	class="theme-toggle"
-	type="submit"
-	title="Toggle theme"
-	aria-label="Toggle theme"
->
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		aria-hidden="true"
-		width="1em"
-		height="1em"
-		class="theme-toggle__simple"
-		fill="currentColor"
-		viewBox="0 0 32 32"
+{#if mounted}
+	<button
+		class:theme-toggle--toggled={theme === Theme.Dark}
+		class="theme-toggle"
+		type="submit"
+		title="Toggle theme"
+		aria-label="Toggle theme"
 	>
-		<clipPath id="theme-toggle__simple__cutout">
-			<path d="M0-5h55v37h-55zm32 12a1 1 0 0025 0 1 1 0 00-25 0" />
-		</clipPath>
-		<g clip-path="url(#theme-toggle__simple__cutout)">
-			<circle cx="16" cy="16" r="15" />
-		</g>
-	</svg>
-</button>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			aria-hidden="true"
+			width="1em"
+			height="1em"
+			class="theme-toggle__simple"
+			fill="currentColor"
+			viewBox="0 0 32 32"
+		>
+			<clipPath id="theme-toggle__simple__cutout">
+				<path d="M0-5h55v37h-55zm32 12a1 1 0 0025 0 1 1 0 00-25 0" />
+			</clipPath>
+			<g clip-path="url(#theme-toggle__simple__cutout)">
+				<circle cx="16" cy="16" r="15" />
+			</g>
+		</svg>
+	</button>
+{/if}
 
 <style>
 	.theme-toggle {
 		font-size: 2em;
 		color: var(--color-foreground);
-		transition: color 150ms ease-in-out;
-		opacity: 0;
-	}
-
-	.theme-toggle--mounted {
-		opacity: 1;
-		transition: opacity 450ms ease-in-out;
-		transition-delay: 150ms;
 	}
 </style>
